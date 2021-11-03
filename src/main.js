77b8f18e5108cdd5484e8123bc5ad57d6a0e1dc2
okd <y.okada.upd@gmail.com>
2021-11-03 13:12:34 +0900

    first deploy

--- src/main.js
+++ src/main.js
@@ -7,7 +7,11 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/storage'
+import 'firebase/database'
+import 'firebase/firestore'
 
 Vue.use(BootstrapVue)
 
